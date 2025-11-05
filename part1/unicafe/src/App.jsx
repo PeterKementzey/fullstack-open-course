f8c06add5a373886e757df9350ff67440f14663a
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2025-11-05 23:43:02 +0100

    feat(unicafe): Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const getTotalValue = () => good - bad
+  const getTotalVotes = () => good + neutral + bad
+
+  return <>
+  <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {getTotalVotes()}</p>
+      <p>Average: {getTotalValue() / getTotalVotes()}</p>
+      <p>Positive: {good / getTotalVotes() * 100} %</p>
+  </>
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -10,22 +26,13 @@ const App = () => {
   const neutralButtonHandler = () => setNeutral(neutral + 1)
   const badButtonHandler = () => setBad(bad + 1)
 
-  const getTotalValue = () => good - bad
-  const getTotalVotes = () => good + neutral + bad
-
   return (
     <div>
       <h1>Give feedback</h1>
       <button onClick={goodButtonHandler}>Good</button>
       <button onClick={neutralButtonHandler}>Neutral</button>
       <button onClick={badButtonHandler}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {getTotalVotes()}</p>
-      <p>Average: {getTotalValue() / getTotalVotes()}</p>
-      <p>Positive: {good / getTotalVotes() * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
