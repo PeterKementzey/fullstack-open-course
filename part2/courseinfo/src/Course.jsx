95ea4a4f4f8559b801fb4842e4f37859072a6481
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2025-11-13 20:54:46 +0100

    feat(courseinfo): Exercise 2.2

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -4,10 +4,15 @@ const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
 const Content = ({ parts }) => parts.map(part => <Part key={part.name} {...part} />)
 
+const Total = ({ parts }) => <p style={{fontWeight: 'bold'}}>
+    Total of {parts.map(part => part.exercises).reduce((total, current) => total + current, 0)} exercises
+</p>
+
 const Course = ({ course }) => {
     return <div>
         <Header course={course.name} />
         <Content parts={course.parts} />
+        <Total parts={course.parts} />
     </div>
 }
 
