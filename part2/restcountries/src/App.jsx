ded2d3afc8a55bb3cce6783815d7445ba92f5867
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2025-11-20 19:42:43 +0100

    feat(restcountries): Exercise 2.18

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -1,5 +1,66 @@
-function App() {
-  return <h1>Hello World!</h1>
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const findMatchingCountries = (countries, countryFilter) => {
+  const matchingCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(countryFilter.toLowerCase()) ||
+    country.name.official.toLowerCase().includes(countryFilter.toLowerCase())
+  )
+  const exactMatch = matchingCountries.find((country) =>
+    country.name.common.toLowerCase() === countryFilter.toLowerCase() ||
+    country.name.official.toLowerCase() === countryFilter.toLowerCase()
+  )
+  return exactMatch ? [exactMatch] : matchingCountries
+}
+
+const CountryInfo = ({ country }) => <>
+  <h1>{country.flag} {country.name.common} {country.flag}</h1>
+  <p>Capital: {country.capital}</p>
+  <p>Area: {country.area} km²</p>
+  <h2>Languages</h2>
+  <ul>{Object.entries(country.languages).map(([abbrev, language]) =>
+    <li key={abbrev}>{language}</li>
+  )}</ul>
+  <img src={country.flags.png} alt={country.flags.alt} />
+</>
+
+const App = () => {
+  const [countryFilter, setCountryFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  const getCountries = () => {
+    axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.error("Could not get countries", error))
+  }
+  useEffect(getCountries, [])
+
+  if (!countries.length) return <p>Loading...</p>
+
+  const matchingCountries = findMatchingCountries(countries, countryFilter)
+
+  let searchResult = <></>
+
+  if (countryFilter === '') {
+    searchResult = <></>
+  } else if (matchingCountries.length > 10) {
+    searchResult = <p>Too many matches, please specify another filter.</p>
+  } else if (matchingCountries.length > 1) {
+    searchResult = <ul>{matchingCountries.map((country) =>
+      <li key={country.cca2}>{country.name.common}</li>
+    )}</ul>
+  } else if (matchingCountries.length == 1) {
+    searchResult = <CountryInfo country={matchingCountries[0]} />
+  } else {
+    searchResult = <p>No matches, please specify another filter.</p>
+  }
+
+  return <>
+    <label>
+      find countries <input type="text" value={countryFilter} onChange={(event) => setCountryFilter(event.target.value)} />
+    </label>
+    {searchResult}
+  </>
 }
 
 export default App
